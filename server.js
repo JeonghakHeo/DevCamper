1027abd0dce1d0f4ef6812766415970ec9299ef1
JeonghakHeo <jeonghak4142@gmail.com>
2021-11-28 21:41:41 +1100

    Initial routes setup

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
+const morgan = require('morgan')
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
-const PORT = process.env.PORT.green.bold || 5000
+// Logging request
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps)
+
+const PORT = process.env.PORT || 5000
 const server = process.env.NODE_ENV.yellow.bold
 
-app.listen(PORT, console.log(`Server running in ${server} on PORT ${PORT}`))
+app.listen(
+  PORT,
+  console.log(`Server running in ${server} mode on PORT ${PORT}`)
+)
