1b58b21a2bcf9d1c81545eb6b56fb6ea66de35cd
JeonghakHeo <jeonghak4142@gmail.com>
2021-11-30 10:40:08 +1100

    First Bootcamp model created

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,3 +13,9 @@ router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
 module.exports = router
+
+/*
+  router.get('/', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all bootcamps'})
+  })
+*/
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
-
-// Route files
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db')
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
 
 const app = express()
 
@@ -18,11 +21,17 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
-
 const PORT = process.env.PORT || 5000
-const server = process.env.NODE_ENV.yellow.bold
+const nodeEnvironment = process.env.NODE_ENV.yellow.bold
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${server} mode on PORT ${PORT}`)
+  console.log(`Server running in ${nodeEnvironment} mode on PORT ${PORT}`)
 )
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  // Close server & exit process
+  server.close(() => process.exit(1))
+})
